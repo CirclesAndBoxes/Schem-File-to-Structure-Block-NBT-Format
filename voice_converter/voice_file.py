eb0276e155a238ddabc9a1568db26d2ace3f051b
Joshua L. <138627372+CirclesAndBoxes@users.noreply.github.com>
2026-02-24 12:47:30 -0600

    Sending voice files and transcripts.

--- voice_converter/voice_file.py
+++ voice_converter/voice_file.py
@@ -44,7 +44,7 @@ texts = [
     "The glass surrounding the yeast cell represents the cell wall—a strong outer layer that surrounds and protects the yeast cell. The yeast cell wall gives the cell its shape, provides mechanical strength, and prevents it from bursting when water enters.",
     "Inside of the cell wall is the cell membrane, represented by lime stained glass. This is a soft, flexible layer that wraps around the cell. It controls which materials that enter and exit the cell.",
     "Continuing to work our way into the cell, we find the endoplasmic reticulum, or ER, represented by orange stained glass. The ER is responsible for transporting proteins and molecules across the cell and helping produce important molecules. ",
-    "The ER is divided into sections based on its locations. The ER by the cell membrane is the peripheral ER. [PAN TO NEW LOCATION] This connects to the tubular ER, which acts as pipes to transport materials across the cell. [KEEP PANNING] the tubular ER connects to the cisternal ER, which is closest to the nucleus and thus takes materials from it.",
+    "The ER is divided into sections based on its locations. The ER by the cell membrane is the peripheral ER. This connects to the tubular ER, which acts as pipes to transport materials across the cell. [KEEP PANNING] the tubular ER connects to the cisternal ER, which is closest to the nucleus and thus takes materials from it.",
     "The nuclear envelope or nuclear membrane of the cell is shown by the red concrete. This protects the yeast cell's nucleus, or the “brain” of the cell. The nucleus contains all of the DNA for the cell, the instructions for creating various proteins and structures of the cell.",
     "The nuclear envelope has holes in it, known as nuclear pores, which control transport into and out of the nucleus.",
     "Blue concrete represents the chromosomes of the yeast cell. Yeast has 16 separate chromosomes, The chromosomes contain DNA, which stores the genetic information of the cell, along with proteins that allow the chromosome to hold its shape.",
@@ -57,6 +57,33 @@ texts = [
 ]
 
 
+texts = [
+"Knowing the shape and function of the different organelles can help medical workers identify abnormalities in cells. These cells are non-cancerous epithelial breast cells.",
+"The light blue glass showing the outer layer of the cell represents the cell membrane of the cell.",
+"These lime structures are various organelles of the epithelial cells, each performing different functions for the cell.",
+"This is a nucleus of the epithelial cell. The pink stained glass represents the nuclear envelope while the purple concrete represents the nucleolus, which produces ribosomes for the cell."
+]
+
+texts = [
+"These are cancerous epithelial cells. Right away we see that the nuclear membrane has a sharper and more needle-like shape compared to the normal cells.",
+"The organelles in the cancerous cells are also more clumped together and expanded.",
+"Significant changes are also seen in the nucleus of the epithelial cells. The nuclear envelope and nucleolus of the cancerous epithelial cells are both enlarged compared to normal epithelial cells.",
+"Being able to tell the changes of various cells allows medical practitioners to tell when your body has diseases and allows them to find effective treatments."
+]
+
+texts = [
+    "This representation of the minimal cell was reconstructed from a computer simulation. Each block is 1.3 nanometers.",
+    "Simulating the minimal cell provides insight into how it works, allowing us to track individual molecules and observe their interactions.",
+    "Lime green glass represents the cell membrane of the minimal cell. The membrane allows small molecules like water and sugar to flow in and out of the cell. Larger materials need to be allowed in by membrane proteins.",
+    "The blue concrete in this cell represents membrane proteins. Membrane proteins are proteins in the membrane which control how the cell interacts with its environment. These membrane proteins are transport proteins which allow certain molecules to enter and exit the cell as needed.",
+    "Molecules that the cells need will be let in through the membrane proteins, just like how we are passing through to enter the cell.",
+    "The inside of the minimal cell contains cytosolic proteins and chromosomes.",
+    "In this case, the chromosomes are represented by glowstone. Chromosomes are structures of organized DNA. If DNA is like a page of instructions teaching the cell how to build a protein, then chromosomes are like books that organize the pages and hold them together.",
+    "The red concrete represents cytosolic proteins. Cytosolic proteins are proteins inside of the cell that perform functions that the cells need to survive.",
+    "Each of these components work together to allow the minimal cell to sustain life."
+]
+
+
 OUTPUT_FILE = "voice_converter/test.mp3"
 
 async def amain() -> None:
@@ -66,7 +93,7 @@ async def amain() -> None:
     
     for i in range(len(texts)):
         text = texts[i]
-        output_path = f"voice_converter/yeast/ryan{i}.mp3"
+        output_path = f"voice_converter/martini/ryan{i}.mp3"
         communicate = edge_tts.Communicate(text, VOICE)
         await communicate.save(output_path)
 
